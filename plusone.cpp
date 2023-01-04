f7d74764d2740c4ab8b354efca09a6f4a8912ae4
Geethika Singareddy <geethika1129@gmail.com>
2023-01-04 14:06:46 +0530

    add

--- plusone.cpp
+++ plusone.cpp
@@ -1,11 +1,12 @@
+//traverse from end
 for(int i=d.size()-1;i>=0;i--)  {
-          if(d[i]<9){
+          if(d[i]<9){  //number less than 9 add 1
               d[i]++;
               return d;
           }
           else{
-              d[i]=0;
+              d[i]=0;  //make it 0 if it is 9 and carrywill be one
           }
       }
-        d.push_back(0);
+        d.push_back(0); //vector is dynamic no need to add size
         d[0]=1;
