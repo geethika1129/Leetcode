f7641bce096b5923b24f1b4e8a3d1503a3cb7a29
Geethika Singareddy <geethika1129@gmail.com>
2023-01-04 10:30:02 +0530

    add

--- intersection of 2 arrays.cpp
+++ intersection of 2 arrays.cpp
@@ -30,3 +30,24 @@ public:
 };
 
 
+///method 2
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+         int n=nums1.size();
+        int m=nums2.size();
+        unordered_map<int,int>ma;
+        vector<int>v;
+        for(int i=0;i<n;i++) ma[nums1[i]]++;
+        for(int i=0;i<m;i++)
+        {
+            if(ma[nums2[i]]>0)
+            {
+                v.push_back(nums2[i]);
+                ma[nums2[i]]--;
+            }
+        }
+        return v;
+    }
+};
