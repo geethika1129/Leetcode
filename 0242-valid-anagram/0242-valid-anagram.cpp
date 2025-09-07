4fb4713beb59257d7b6796457946fb6bc5aa196d
Geethika <83590629+geethika1129@users.noreply.github.com>
2025-09-07 13:21:15 +0530

    Time: 9 ms (8.12%), Space: 9.7 MB (48.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t)
-        return true;   
-        else return false; }
+        if(s.size()!=t.size()) return false;
+        map<int,int>ms;
+        map<int,int>ts;
+        for(int i=0;i<s.size();i++){
+            ms[s[i]-'a']++;
+            ts[t[i]-'a']++;
+        }
+        return ms==ts;
+    }
 };
